a4efc4f4394191cfb29b0f6be369c7380be36001
Juyoung <wdd1016@naver.com>
2022-12-19 23:27:38 +0900

    12.19i

--- acmicpc/Segment Tree/1395_switch.c
+++ acmicpc/Segment Tree/1395_switch.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int	update_rev(int start, int end, int index, int left, \
+static void	update_rev(int start, int end, int index, int left, \
 int right, int *tree, int *lazy);
 static int sum_tree(int start, int end, int index, int left, \
 int right, int *tree, int *lazy);
-static void turnover_lazy(int start, int mid, int end, \
+static void update_lazy(int start, int end, \
 int index, int *tree, int *lazy);
 
 int main()
@@ -31,79 +31,63 @@ int main()
 		if (inf == 0)
 			update_rev(1, s_count, 1, left, right, tree, lazy);
 		else
-			{
-				k = sum_tree(1, s_count, 1, left, right, tree, lazy);
-				printf("%d\n", k);
-			}
-	
+			printf("%d\n", sum_tree(1, s_count, 1, left, right, tree, lazy));
 	}
 	return 0;
 }
 
-static int	update_rev(int start, int end, int index, int left, int right, int *tree, \
+static void	update_rev(int start, int end, int index, int left, int right, int *tree, \
 int *lazy) // start, end : 함수내 시작과 끝 arr[]idx, left, right : 판별 기준 arr[]idx, index : 현재 tree[]idx
 {
-	int mid = (start + end) / 2;
-	if (lazy[index] == 1)
-	{
-		turnover_lazy(start, mid, end, index, tree, lazy);
-		//lazy를 아래 노드에 전달
-	}
+	update_lazy(start, end, index, tree, lazy);
 	if (right < start || end < left) // 범위에 벗어나는 경우
-		return tree[index];
+		return;
 	else if (left <= start && end <= right) // 함수범위가 기준범위내에 포함되는 경우
 	{
-		if (start == end) // 끝에 도달하는 경우
+		tree[index] = (end - start + 1) - tree[index]; // 해당 노드 반전
+		if (start != end)
 		{
-			tree[index] = !tree[index]; // 반전
-			return tree[index];
+			lazy[index * 2] += 1; // 하위노드 lazy 추가
+			lazy[index * 2 + 1] += 1;
 		}
-		else
-		{
-			lazy[index] = !lazy[index]; // lazy는 반전(0 <-> 1)만 기록하므로 2번적용시 원래상태 저장
-			tree[index] = (end - start + 1) - tree[index]; // 전체개수에 대한 반전
-			return tree[index];
-		}
+		return;
 	}
 	else
 	{
-		tree[index] = update_rev(start, mid, index * 2, left, right, tree, lazy) + \
+		int mid = (start + end) / 2;
+		update_rev(start, mid, index * 2, left, right, tree, lazy);
 		update_rev(mid + 1, end, index * 2 + 1, left, right, tree, lazy);
-		return tree[index];
+		tree[index] = tree[index * 2] + tree[index * 2 + 1];
+		return;
 	}
 }
 
 static int sum_tree(int start, int end, int index, int left, \
 int right, int *tree, int *lazy)
 {
-	int mid = (start + end) / 2;
-	if (lazy[index] == 1)
-	{
-		turnover_lazy(start, mid, end, index, tree, lazy);
-		//lazy를 아래 노드에 전달		
-	}
+	update_lazy(start, end, index, tree, lazy);
 	if (right < start || end < left) // 함수범위가 기준범위에서 벗어나는 경우
 		return 0;
 	else if (left <= start && end <= right) // 함수범위가 기준범위내에 포함되는 경우
 		return tree[index];
 	else
 	{
+		int mid = (start + end) / 2;
 		return (sum_tree(start, mid, index * 2, left, right, tree, lazy) + \
 		sum_tree(mid + 1, end, index * 2 + 1, left, right, tree, lazy));
 	}
 }
 
-static void turnover_lazy(int start, int mid, int end, int index, int *tree, int *lazy)
+static void update_lazy(int start, int end, int index, int *tree, int *lazy)
 {
+	if (lazy[index] % 2 == 1) // 값에 lazy적용
+		tree[index] = (end - start + 1) - tree[index];
+	if (start != end)
+	{
+		lazy[index * 2] += lazy[index];
+		lazy[index * 2 + 1] += lazy[index]; 
+	}
 	lazy[index] = 0; // lazy를 물려주므로 이제 0의 값을 가짐
-
-	if (start != mid)
-		lazy[index * 2] = 1;
-	if (mid + 1 != end)
-		lazy[index * 2 + 1] = 1; // lazy 물려줌
-
-	tree[index * 2] = (mid - start + 1) - tree[index * 2];
-	tree[index * 2 + 1] = (end - mid) - tree[index * 2 + 1]; // lazy 받았으니 적용 (line 48~49)
 }
 
 /* 아래 내용은 lazy_Propagation이 구현되지 않아, 타임에러가 발생
