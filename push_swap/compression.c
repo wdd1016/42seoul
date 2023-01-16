493f1ac94c717d912abb1efce0e5e5977edb80af
Juyoung <wdd1016@naver.com>
2023-01-16 23:23:23 +0900

    compression

--- push_swap/compression.c
+++ push_swap/compression.c
@@ -6,7 +6,7 @@
 /*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 23:56:21 by juyojeon          #+#    #+#             */
-/*   Updated: 2023/01/16 20:08:28 by juyojeon         ###   ########.fr       */
+/*   Updated: 2023/01/16 23:23:12 by juyojeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,18 @@ void	ft_coor_compression(t_stacks *stk)
 	temparr = (element *)malloc(sizeof(element) * stk->size);
 	if (!temparr)
 		ft_error_ps(stk);
-	i = -1;
-	while (++i < stk->size)
+	i = 0;
+	while (i < stk->size)
+	{
 		temparr[i] = (stk->array)[i];
+		i++;
+	}
 	ft_timsort(temparr, 0, stk->size - 1);
-
+	i = 0;
+	while (i < stk->size)
+	{
+		(stk->array)[i] = ft_findindex(temparr, (stk->array)[i], stk->size);
+		i++;
+	}
+	free(temparr);
 }
