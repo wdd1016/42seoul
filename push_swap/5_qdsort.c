caddc5f3ab00e9f2e3a30da7e8c88f55aa26d106
Juyoung <wdd1016@naver.com>
2023-01-18 22:57:21 +0900

    finish??

--- push_swap/5_qdsort.c
+++ push_swap/5_qdsort.c
@@ -6,7 +6,7 @@
 /*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 04:20:13 by juyojeon          #+#    #+#             */
-/*   Updated: 2023/01/18 18:49:42 by juyojeon         ###   ########.fr       */
+/*   Updated: 2023/01/18 22:57:21 by juyojeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,16 +37,19 @@ static void	ft_asorting(t_stacks *stk, t_procstk *now)
 	int		count;
 
 	count = now->max - now->min + 1;
-	if (count <= 2)
-		ft_hardsorting(stk, A, count);
-	info.rra = 0;
-	info.rrb = 0;
-	info.pivot1 = now->min + (now->max - now->min) / 3;
-	info.pivot2 = now->max - (now->max - now->min) / 3;
-	ft_aordering(stk, &info, count);
-	ft_pushstack(stk, B, now->min, info.pivot1 - 1);
-	ft_pushstack(stk, B, info.pivot1, info.pivot2);
-	ft_pushstack(stk, A, info.pivot2 + 1, now->max);
+	if (count <= 5)
+		ft_hardsorting(stk, count, now);
+	else
+	{
+		info.rra = 0;
+		info.rrb = 0;
+		info.pivot1 = now->min + (now->max - now->min) / 3;
+		info.pivot2 = now->max - (now->max - now->min) / 3;
+		ft_aordering(stk, &info, count);
+		ft_pushstack(stk, B, now->min, info.pivot1 - 1);
+		ft_pushstack(stk, B, info.pivot1, info.pivot2);
+		ft_pushstack(stk, A, info.pivot2 + 1, now->max);
+	}
 }
 // pivot1 : take small value stack
 // pivot2 : take big value stack
@@ -85,26 +88,29 @@ static void	ft_bsorting(t_stacks *stk, t_procstk *now)
 	int		count;
 
 	count = now->max - now->min + 1;
-	if (count <= 2)
-		ft_hardsorting(stk, B, count);
-	info.rra = 0;
-	info.rrb = 0;
-	info.pivot1 = now->min + count / 3;
-	info.pivot2 = now->max - count / 3;
-	ft_bordering(stk, &info, count);
-	ft_pushstack(stk, A, info.pivot2 + 1, now->max);
-	ft_qdsort(stk, stk->top - 1);
-	count = 0;
-	while (count++ < MIN(info.rra, info.rrb))
-		doubleorder(stk, RRR);
-	if (info.rra > info.rrb)
-		while (count++ < info.rra)
-			reverseorder(stk, RRA, stk->a);
-	else if (info.rra < info.rrb)
-		while (count++ < info.rrb)
-			reverseorder(stk, RRB, stk->b);
-	ft_pushstack(stk, B, now->min, info.pivot1 - 1);
-	ft_pushstack(stk, A, info.pivot1, info.pivot2);
+	if (count <= 5)
+		ft_hardsorting(stk, count, now);
+	else
+	{
+		info.rra = 0;
+		info.rrb = 0;
+		info.pivot1 = now->min + count / 3;
+		info.pivot2 = now->max - count / 3;
+		ft_bordering(stk, &info, count);
+		ft_pushstack(stk, A, info.pivot2 + 1, now->max);
+		ft_qdsort(stk, stk->top - 1);
+		count = 0;
+		while (count++ < MIN(info.rra, info.rrb))
+			doubleorder(stk, RRR);
+		if (info.rra > info.rrb)
+			while (count++ < info.rra)
+				reverseorder(stk, RRA, stk->a);
+		else if (info.rra < info.rrb)
+			while (count++ < info.rrb)
+				reverseorder(stk, RRB, stk->b);
+		ft_pushstack(stk, B, now->min, info.pivot1 - 1);
+		ft_pushstack(stk, A, info.pivot1, info.pivot2);
+	}
 }
 // pivot1 : take midium value stack
 // pivot2 : take midium value stack
