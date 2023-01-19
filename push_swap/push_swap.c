ce8063b40dbf26017c43a43abe4ffd3ee5532727
Juyoung <wdd1016@naver.com>
2023-01-20 05:25:05 +0900

    ing

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,13 +6,14 @@
 /*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 18:05:24 by juyojeon          #+#    #+#             */
-/*   Updated: 2023/01/20 04:09:21 by juyojeon         ###   ########.fr       */
+/*   Updated: 2023/01/20 05:25:05 by juyojeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 static void	ft_make_stacks(t_stacks *stk);
+static void	ft_freeall(t_stacks *stk);
 
 int	main(int argc, char *argv[])
 {
@@ -26,22 +27,50 @@ int	main(int argc, char *argv[])
 	ft_make_array(&stk, argc, argv);
 	ft_coor_compression(&stk);
 	ft_make_stacks(&stk);
-	ft_pushstack(&stk, A, 0, stk.size - 1);
-	ft_qdsort(&stk, -1);
-	if (stk.array)
-		free(stk.array);
-	if (stk.a)
-		free(stk.a);
-	if (stk.b)
+	if (stk.size <= 5)
 	{
-		if (stk.b->data)
-			free(stk.b->data);
-		free(stk.b);
+
 	}
-	if (stk.stack)
-		free(stk.stack);
+	else
+	{
+		ft_pushstack(&stk, A, 0, stk.size - 1);
+		ft_qdsort(&stk, -1);
+	}
+	ft_freeall(&stk);
 	return (0);
 }
+
+void	ft_pushstack(t_stacks *stk, int n_location, int n_min, int n_max)
+{
+	t_procstk	*topstack;
+
+	stk->top += 1;
+	topstack = &((stk->stack)[stk->top]);
+	topstack->location = n_location;
+	topstack->max = n_max;
+	topstack->min = n_min;
+}
+
+static void	ft_make_stacks(t_stacks *stk)
+{
+	stk->a = (t_deque *)malloc(sizeof(t_deque));
+	if (stk->a == NULL)
+		ft_error_ps(stk);
+	stk->a->data = stk->array;
+	stk->a->front = 0;
+	stk->a->rear = stk->size - 1;
+	stk->b = (t_deque *)malloc(sizeof(t_deque));
+	if (stk->b == NULL)
+		ft_error_ps(stk);
+	stk->b->data = (t_elemt *)malloc(sizeof(t_elemt) * stk->size);
+	if (stk->b->data == NULL)
+		ft_error_ps(stk);
+	stk->b->front = 1;
+	stk->b->rear = 0;
+	stk->stack = (t_procstk *)malloc(sizeof(t_procstk) * stk->size);
+	stk->top = -1;
+}
+// when PB, front -= -1, therefore init b->front "1" && b ->rear "0"
 
 void	ft_error_ps(t_stacks *stk)
 {
@@ -61,39 +90,18 @@ void	ft_error_ps(t_stacks *stk)
 	exit(1);
 }
 
-static void	ft_make_stacks(t_stacks *stk)
+static void	ft_freeall(t_stacks *stk)
 {
-	stk->a = (t_deque *)malloc(sizeof(t_deque));
-	if (stk->a == NULL)
-		ft_error_ps(stk);
-	stk->a->data = stk->array;
-	stk->a->front = 0;
-	stk->a->rear = stk->size - 1;
-	stk->b = (t_deque *)malloc(sizeof(t_deque));
-	if (stk->b == NULL)
-		ft_error_ps(stk);
-	stk->b->data = (t_elemt *)malloc(sizeof(t_elemt) * stk->size);
-	if (stk->b->data == NULL)
-		ft_error_ps(stk);
-	stk->b->front = 1; // when push(PB), front -> front -1, init b->front "1" && b ->rear "0"
-	stk->b->rear = 0;
-	stk->stack = (t_procstk *)malloc(sizeof(t_procstk) * stk->size);
-	stk->top = -1;
-}
-
-void	ft_pushstack(t_stacks *stk, int n_location, int n_min, int n_max)
-{
-	t_procstk	*topstack;
-
-	stk->top += 1;
-	topstack = &((stk->stack)[stk->top]);
-	topstack->location = n_location;
-	topstack->max = n_max;
-	topstack->min = n_min;
-}
-
-t_procstk	ft_popstack(t_stacks *stk)
-{
-	stk->top -= 1;
-	return ((stk->stack)[stk->top + 1]);
+	if (stk->array)
+		free(stk->array);
+	if (stk->a)
+		free(stk->a);
+	if (stk->b)
+	{
+		if (stk->b->data)
+			free(stk->b->data);
+		free(stk->b);
+	}
+	if (stk->stack)
+		free(stk->stack);	
 }
