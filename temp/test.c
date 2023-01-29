b70db81fd816c0238ce9c7177a5b8377042cd3e3
Juyoung <wdd1016@naver.com>
2023-01-30 04:05:36 +0900

    test

--- temp/test.c
+++ temp/test.c
@@ -6,13 +6,14 @@
 /*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 17:08:06 by juyojeon          #+#    #+#             */
-/*   Updated: 2023/01/29 19:16:40 by juyojeon         ###   ########.fr       */
+/*   Updated: 2023/01/30 03:23:55 by juyojeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include <stdio.h>
 
 int main(void)
 {
-	ft_putwchar_fd(0xBA51, 1);
+	int i;
+	scanf("%d", &i);
 }
