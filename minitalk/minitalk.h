8d4c474bc3164ef1083e24c4a6014d30746a1f29
Juyoung <juyojeon@student.42seoul.kr>
2023-01-30 22:41:52 +0900

    ok

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/29 03:27:25 by juyojeon          #+#    #+#             */
-/*   Updated: 2023/01/30 16:10:01 by juyojeon         ###   ########.fr       */
+/*   Created: 2023/01/30 16:08:06 by juyojeon          #+#    #+#             */
+/*   Updated: 2023/01/30 16:20:08 by juyojeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#ifndef MINITALK_H
+# define MINITALK_H
 
-int	main(void)
-{
-	
-}
+# include "ftprintf/ft_printf.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <signal.h>
+
+# define TRUE 1
+# define FALSE 0
+
+#endif
--- minitalk/test/client copy.c
+++ minitalk/test/client copy.c
@@ -6,33 +6,41 @@
 /*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 00:08:06 by juyojeon          #+#    #+#             */
-/*   Updated: 2023/01/30 22:41:47 by juyojeon         ###   ########.fr       */
+/*   Updated: 2023/01/30 20:48:25 by juyojeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h>
 
-struct sigaction act;
+void	handler(int signo, siginfo_t *info, void *content)
+{
+	printf("\n%d\n", 111);
+}
+
+void	handler2(int signo)
+{
+	printf("\n%d\n", 222);
+}
 
 int	main(int argc, char *argv[])
 {
-	act.sa_sigaction = check_client;
+	pid_t	mypid;
+	struct sigaction act;
+
+	act.sa_sigaction = handler;
 	act.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &act, NULL);
-	printf("%d\n", getpid());
-	while (TRUE);
+	mypid = getpid();
+	printf("%d\n", mypid);
+	sigaction(SIGINT, &act, NULL);
+	pause();
+	act.sa_handler = handler2;
+	sigaction(SIGINT, &act, NULL);
+	printf("%d\n", 999);
+	// act.sa_flags = 0;
+	while (TRUE)
+	{
+		pause();
+		printf("%d\n", 888);
+	}
 }
-
-void	check_client(int signo, siginfo_t *info, void *content)
-{
-	info->
-}
-
-void	process_str(int signo, siginfo_t *info, void *content)
-{
-	static unsigned char	buf[];
-	if ()
-}
-
-
