56529c70bbae0171687dec376ae2e14c3429a5c4
Juyoung <juyojeon@student.42seoul.kr>
2023-02-27 18:42:23 +0900

    start mandatory

---
+++ philosophers/philo/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 18:37:59 by juyojeon          #+#    #+#             */
+/*   Updated: 2023/02/27 18:42:23 by juyojeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <string.h>
+# include <unistd.h>
+# include <pthread.h>
+
+#endif
