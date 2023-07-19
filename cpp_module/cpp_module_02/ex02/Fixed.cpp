fe98ed75f1a0dfda994d8120ed799085baa1af80
Juyoung <juyojeon@student.42seoul.kr>
2023-07-19 23:49:23 +0900

    ADD ex02 except multiple, divide

---
+++ cpp_module/cpp_module_02/ex02/Fixed.cpp
@@ -0,0 +1,153 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/17 22:23:11 by juyojeon          #+#    #+#             */
+/*   Updated: 2023/07/19 23:48:53 by juyojeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _value(0) {
+  std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& copy) {
+  std::cout << "Copy constructor called" << std::endl;
+  *this = copy;
+}
+
+Fixed::Fixed(const int initValue) : _value(initValue << _fractionBit) {
+  std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float initValue) : _value(roundf(initValue * (1 << _fractionBit))) {
+  std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::~Fixed() {
+std::cout << "Destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& copy) {
+  std::cout << "Copy assignment operator called" << std::endl;
+  if (this != &copy)
+    this->_value = copy._value;
+  return (*this);
+}
+
+bool  Fixed::operator>(const Fixed& target) const {
+  return (this->_value > target._value ? true : false);
+}
+
+bool  Fixed::operator<(const Fixed& target) const {
+  return (this->_value < target._value ? true : false);
+}
+
+bool  Fixed::operator>=(const Fixed& target) const {
+  return (this->_value >= target._value ? true : false);
+}
+
+bool  Fixed::operator<=(const Fixed& target) const {
+  return (this->_value <= target._value ? true : false);
+}
+
+bool  Fixed::operator==(const Fixed& target) const {
+  return (this->_value == target._value ? true : false);
+}
+
+bool  Fixed::operator!=(const Fixed& target) const {
+  return (this->_value != target._value ? true : false);
+}
+
+Fixed Fixed::operator+(const Fixed& target) const {
+  Fixed temp;
+  
+  temp._value = this->getRawBits() + target.getRawBits();
+  return (temp);
+}
+
+Fixed Fixed::operator-(const Fixed& target) const {
+  Fixed temp;
+
+  temp._value = this->getRawBits() - target.getRawBits();
+  return (temp);
+}
+
+// Fixed Fixed::operator*(const Fixed& target) const {
+//   Fixed temp;
+
+//   temp._value = this->getRawBits() * target.getRawBits();
+//   return (temp);
+// }
+
+// Fixed Fixed::operator/(const Fixed& target) const {
+//   Fixed temp;
+
+//   temp._value = this->getRawBits() / target.getRawBits();
+//   return (temp);
+// }
+
+Fixed&  Fixed::operator++() {
+  this->_value++;
+  return (*this);
+}
+
+Fixed&  Fixed::operator--() {
+  this->_value--;
+  return (*this);
+}
+
+Fixed  Fixed::operator++(int) {
+  Fixed temp(*this);
+  this->_value++;
+  return (temp);
+}
+
+Fixed  Fixed::operator--(int) {
+  Fixed temp(*this);
+  this->_value--;
+  return (temp);
+}
+
+int Fixed::getRawBits() const {
+  std::cout << "getRawBits member function called" << std::endl;
+  return (this->_value);
+}
+
+void Fixed::setRawBits(int const raw) {
+  this->_value = raw;
+}
+
+float Fixed::toFloat(void) const {
+  return (static_cast<float>(this->_value) / (1 << _fractionBit));
+}
+
+int Fixed::toInt() const {
+  return (this->_value / (1 << _fractionBit));
+}
+
+Fixed& Fixed::min(Fixed& one, Fixed& two) {
+  return (one.getRawBits() <= two.getRawBits() ? one : two);
+}
+
+const Fixed& Fixed::min(const Fixed& one, const Fixed& two) {
+  return (one.getRawBits() <= two.getRawBits() ? one : two);
+}
+
+Fixed& Fixed::max(Fixed& one, Fixed& two) {
+  return (one.getRawBits() >= two.getRawBits() ? one : two);
+}
+
+const Fixed& Fixed::max(const Fixed& one, const Fixed& two) {
+  return (one.getRawBits() >= two.getRawBits() ? one : two);
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& addValue) {
+  os << addValue.toFloat();
+  return (os);
+}
---
+++ cpp_module/cpp_module_02/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 19:14:40 by juyojeon          #+#    #+#             */
+/*   Updated: 2023/07/19 22:13:41 by juyojeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main() {
+  Fixed a;
+  Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+  std::cout << a << std::endl;
+  std::cout << ++a << std::endl;
+  std::cout << a << std::endl;
+  std::cout << a++ << std::endl;
+  std::cout << a << std::endl;
+
+  std::cout << b << std::endl;
+
+  std::cout << Fixed::max(a, b) << std::endl;
+
+  return 0;
+}
