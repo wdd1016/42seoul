b6d082ab46e97bc4acc69d3c48b0a7d5b2a6aef6
Juyoung <juyojeon@student.42seoul.kr>
2023-07-27 23:57:01 +0900

    [Edit] CPP03 ex01 July 27

--- cpp_module/cpp_module_03/ex01/ClapTrap.hpp
+++ cpp_module/cpp_module_03/ex01/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 20:04:06 by juyojeon          #+#    #+#             */
-/*   Updated: 2023/07/27 19:00:42 by juyojeon         ###   ########.fr       */
+/*   Updated: 2023/07/27 23:27:37 by juyojeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 
 #include <iostream>
 
-class ClapTrap
-{
+class ClapTrap {
+
 protected:
-	const std::string _name;
-  int _hitPoint;
-  int _energyPoint;
-  int _attackDamege;
+	std::string _name;
+  unsigned int _hitPoint;
+  unsigned int _energyPoint;
+  unsigned int _attackDamege;
 
 public:
 	ClapTrap(const std::string name);
 	~ClapTrap();
   ClapTrap(const ClapTrap& copy);
-  ClapTrap  operator=(const ClapTrap& copy);
+  ClapTrap& operator=(const ClapTrap& copy);
 
   void  attack(const std::string& target);
   void  takeDamage(unsigned int amount);
--- cpp_module/cpp_module_03/ex01/ScavTrap.cpp
+++ cpp_module/cpp_module_03/ex01/ScavTrap.cpp
@@ -1,31 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/27 22:51:24 by juyojeon          #+#    #+#             */
-/*   Updated: 2023/07/27 23:56:33 by juyojeon         ###   ########.fr       */
+/*   Created: 2023/07/27 22:51:11 by juyojeon          #+#    #+#             */
+/*   Updated: 2023/07/27 23:56:32 by juyojeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCAVTRAP_HPP
-#define SCAVTRAP_HPP
+#include "ScavTrap.hpp"
 
-#include <iostream>
-#include "ClapTrap.hpp"
+ScavTrap::ScavTrap(const std::string name) : ClapTrap(name), 
+_hitPoint(100), _energyPoint(50), _attackDamege(20) {
+  std::cout << "ScavTrap " << _name << " is generated." << std::endl;
+}
 
-class ScavTrap : public ClapTrap {
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap " << _name << " is destroyed." << std::endl;
+}
 
-protected:
-  unsigned int _hitPoint;
-  unsigned int _energyPoint;
-  unsigned int _attackDamege;
-  
-public:
-	ScavTrap(const std::string name);
-	~ScavTrap();
-};
-
-#endif
