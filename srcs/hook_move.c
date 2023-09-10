f2f4ebf9feefa2ae5b91ccdff3a69e22ef649ce6
Juyoung <juyojeon@student.42seoul.kr>
2023-09-10 21:52:29 +0900

    [FIX] norminette

--- srcs/hook_move.c
+++ srcs/hook_move.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hook_move.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jiyeolee <jiyeolee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 21:42:16 by jiyeolee          #+#    #+#             */
-/*   Updated: 2023/09/10 21:49:21 by jiyeolee         ###   ########.fr       */
+/*   Updated: 2023/09/10 21:51:32 by juyojeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ void	move_updown_player(int key, t_player *p, char **map)
 {
 	if (key == KEY_W)
 	{
- 		if (map[(int)(p->y + p->dir_y * p->walk_speed)][(int)p->x] == '0')
+		if (map[(int)(p->y + p->dir_y * p->walk_speed)][(int)p->x] == '0')
 			p->y += p->dir_y * p->walk_speed;
-      	if (map[(int)p->y][(int)(p->x + p->dir_x * p->walk_speed)] == '0')
+		if (map[(int)p->y][(int)(p->x + p->dir_x * p->walk_speed)] == '0')
 			p->x += p->dir_x * p->walk_speed;
 	}
 	else if (key == KEY_S)
 	{
- 		if (map[(int)(p->y - p->dir_y * p->walk_speed)][(int)p->x] == '0')
+		if (map[(int)(p->y - p->dir_y * p->walk_speed)][(int)p->x] == '0')
 			p->y -= p->dir_y * p->walk_speed;
-      	if (map[(int)p->y][(int)(p->x - p->dir_x * p->walk_speed)] == '0')
+		if (map[(int)p->y][(int)(p->x - p->dir_x * p->walk_speed)] == '0')
 			p->x -= p->dir_x * p->walk_speed;
 	}
 }
