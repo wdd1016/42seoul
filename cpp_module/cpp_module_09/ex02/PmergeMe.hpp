5755ba06986eab8228a336b6d0cda2648afbffa5
Juyoung <wdd1016@naver.com>
2023-11-19 03:09:57 +0900

    [Edit]  Ford-Johnson algorithm

--- cpp_module/cpp_module_09/ex02/PmergeMe.hpp
+++ cpp_module/cpp_module_09/ex02/PmergeMe.hpp
@@ -23,7 +23,7 @@ class PmergeMe {
     nextEnd = pairComparison(numbers, elementSize, pairCount);
     if (pairCount != 1)
       fordJohnsonSort(numbers, nextEnd, elementSize * 2, pairCount / 2);
-    pendingElementInsertion(numbers, end, elementSize, pairCount);
+    remainedElementsInsertion(numbers, end, elementSize, pairCount);
   }
 
   Container::iterator pairComparison(Container &numbers, size_t elementSize,
@@ -41,9 +41,55 @@ class PmergeMe {
     return fir;
   }
 
-  void pendingElementInsertion(Container &numbers, Container::iterator end,
-                               size_t elementSize, size_t pairCount) {
-    Container temp;
+  void remainedElementsInsertion(Container &numbers, Container::iterator end,
+                                 size_t elementSize, size_t pairCount) {
+    Container largerElements(elementSize * pairCount);
+    Container remainedElements(elementSize * pairCount);
+    Container::iterator numbersIt = numbers.begin();
+    Container::iterator largeIt = largerElements.begin();
+    Container::iterator remainIt = remainedElements.begin();
+    Container::iterator tempIt;
+
+    for (size_t i = 0; i < pairCount; i++) {
+      tempIt = numbersIt + elementSize;
+      largeIt = std::copy(numbersIt, tempIt, largeIt);
+      numbersIt = tempIt;
+      tempIt = numbersIt + elementSize;
+      remainIt = std::copy(numbersIt, tempIt, remainIt);
+      numbersIt = tempIt;
+    }
+    if (numbersIt != end)
+      remainedElements.insert(remainedElements.end(), numbersIt, end);
+    remainIt = remainedElements.begin();
+    while (remainIt != remainedElements.end()) {
+      tempIt = remainIt + elementSize;
+      binarySearchInsertion(largerElements, remainIt, tempIt, elementSize);
+      remainIt = tempIt;
+    }
+    std::copy(largerElements.begin(), largerElements.end(), numbers.begin());
+  }
+
+  void binarySearchInsertion(Container &lgElements, Container::iterator &compIt,
+                             Container::iterator &endIt, size_t elementSize) {
+    int compNumber = *compIt;
+    size_t low = 0;
+    size_t high = (lgElements.size() / elementSize) - 1;
+    size_t mid;
+
+    if (compNumber <= lgElements[0]) {
+      lgElements.insert(lgElements.begin(), compIt, endIt);
+      return;
+    }
+    while (low <= high) {
+      mid = (low + high / 2);
+      if (compNumber > lgElements[mid * elementSize])
+        high = mid - 1;
+      else {
+        lgElements.insert(lgElements.begin() + mid * elementSize, compIt,
+                          endIt);
+        return;
+      }
+    }
   }
 };
 
