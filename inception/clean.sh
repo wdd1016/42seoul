6d5c57749fb3d3a12a54f6c5d5a346594308fd0b
Juyoung <juyojeon@student.42seoul.kr>
2024-01-10 22:33:56 +0900

    [ADD] Inception nginx add, Directory change

---
+++ inception/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker stop $(docker ps -qa) 2>/dev/null
+docker rm $(docker ps -qa) 2>/dev/null
+docker rmi -f $(docker images -qa) 2>/dev/null
+docker volume rm $(docker volume ls -q) 2>/dev/null
+docker network rm $(docker network ls -q) 2>/dev/null
